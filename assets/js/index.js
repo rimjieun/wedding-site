18fac0831e30cdf7ea24f64a9d3f0ee93e6ea7d3
Jieun Rim <rim.jieun@gmail.com>
2018-01-16 23:25:01 -0500

    save changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,34 +22,6 @@ $(document).ready(function() {
     }
   });
 
-  // $("#intro-btn, .side-nav#intro-btn").click(function() {
-  //   $('html,body').animate({
-  //       scrollTop: $(".section2").offset().top
-  //     },
-  //     'slow');
-  // });
-
-  // $("#skills-btn, .side-nav#intro-btn").click(function() {
-  //   $('html,body').animate({
-  //       scrollTop: $(".section3").offset().top
-  //     },
-  //     'slow');
-  // });
-
-  // $("#portfolio-btn").click(function() {
-  //   $('html,body').animate({
-  //       scrollTop: $(".section4").offset().top
-  //     },
-  //     'slow');
-  // });
-
-  // $("#contact-btn").click(function() {
-  //   $('html,body').animate({
-  //       scrollTop: $(".section5").offset().top
-  //     },
-  //     'slow');
-  // });
-
   $('a[href^="#"]').on('click', function (event) {
     event.preventDefault();
 
