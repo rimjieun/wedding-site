80f265118ab921b29d6933746d304ff989a5bb62
Jieun Rim <rim.jieun@gmail.com>
2018-02-18 23:16:54 -0500

    saving changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -107,6 +107,23 @@ $(window).on('load', function() {
 
 });
 
+var ww = $(window).width();
+var limit = 600;
+
+function refresh() {
+  ww = $(window).width();
+  var w = ww < limit ? (location.reload(true)) : (ww > limit ? (location.reload(true)) : ww = limit);
+}
+
+var tOut;
+
+
 $(window).resize(function(e) {
   $('#cycler').height($('img.size-ref').height());
+  // var sizeChanged = false;
+  var resW = $(window).width();
+  clearTimeout(tOut);
+  if ((ww > limit && resW < limit) || (ww < limit && resW > limit)) {
+    tOut = setTimeout(refresh, 100);
+  }
 });
