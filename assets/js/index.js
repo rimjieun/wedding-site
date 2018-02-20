c1dc7f28b1b958e45b283bcc99bcb163e89d3da6
Jieun Rim <rim.jieun@gmail.com>
2018-02-20 02:28:33 -0500

    activities mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -141,8 +141,7 @@ for (i = 0; i < acc.length; i++) {
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
-      var newHeight = panel.scrollHeight + 20;
-      panel.style.maxHeight = newHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   });
 }
